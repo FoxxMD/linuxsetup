90b4c715ed7ed224a4d826ae10e0ff9cad9811fe
FoxxMD <FoxxMD@users.noreply.github.com>
2022-09-02 11:00:34 -0400

    Add some useful scripts

---
+++ scripts/journalClean.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ # on Ubuntu
+ # reduce journal size to megabytes
+journalctl --vacuum-size=500M
---
+++ scripts/snapClean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ # https://superuser.com/a/1330590/359383
+ #Removes old revisions of snaps
+ #CLOSE ALL SNAPS BEFORE RUNNING THIS
+ set -eu
+ LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
+     while read snapname revision; do
+         snap remove "$snapname" --revision="$revision"
+     done
